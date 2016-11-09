2d1e56a9438fbbeb1702b1795a3f3d7928ac71f8
anmol678 <anmolj678@gmail.com>
2016-11-08 22:37:53 -0800

    Added Player class definition

--- player.cpp
+++ player.cpp
@@ -1,1 +1,21 @@
 //player.cpp
+
+#include "player.h"
+#include "hand.h"
+#include "cards.h"
+
+//Default constructor intializes m amount of money
+Player::Player(int m) {
+    money = m;
+    PlayerHand = Hand();
+}
+
+//Accessor: Returns the amount of money
+int Player::getMoney() const {
+    return money;
+}
+
+//Accessor: Updates money player has based on bets won or lost
+void Player::updateMoney(int change) {
+    money += change;
+}
